c78234e679a583f5d7b47d4b6f931e1cea9d0d26
sewon min <nvpz1598@gmail.com>
2023-08-03 00:44:21 +0900

    #23 fix: todo모델 수정-content 필드 삭제

--- server/apps/account/admin.py
+++ server/apps/account/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-
+admin.site.register(Value)
+admin.site.register(Todolist)
+admin.site.register(Todo)
 # Register your models here.
--- server/apps/main/models.py
+++ server/apps/main/models.py
@@ -1,3 +1,4 @@
 from django.db import models
+from server.apps.account.models import *
 
-# Create your models here.
+
