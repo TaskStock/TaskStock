40099ee88acd1e7b7a1d9af7fb503ca1213d7004
ajroot5685 <anjeong8577@naver.com>
2023-08-02 22:28:02 +0900

    chore: 모델 추가 위한 임시 커밋

--- server/config/settings.py
+++ server/config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'server.apps.account'
+    'server.apps.account',
 ]
 
 MIDDLEWARE = [
