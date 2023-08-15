7e86a9b439492c5cb3cde8e34164df3b9709073f
ajroot5685 <anjeong8577@naver.com>
2023-08-15 23:02:24 +0900

    chore: db 작업

--- server/config/settings.py
+++ server/config/settings.py
@@ -29,8 +29,11 @@ environ.Env.read_env(BASE_DIR / '../.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+import my_settings
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = my_settings.SECRET_KEY
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -101,12 +104,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = my_settings.DATABASES
 
 
 # Password validation
