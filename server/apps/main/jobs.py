3e5ca28d3a6c8fa2c32cc51882798144f7b5c784
sewon min <nvpz1598@gmail.com>
2023-08-15 17:24:38 +0900

    #236 fix: 오늘 아닌 날에 add_todo했을 때 오늘의 high, low가 변하도록 수정

--- server/apps/main/jobs.py
+++ server/apps/main/jobs.py
@@ -3,31 +3,31 @@ from django_apscheduler.jobstores import DjangoJobStore, register_events, regist
 from server.apps.task_account.models import *
 from .views import *
 import datetime
+import arrow
 
 
 
 scheduler = BackgroundScheduler()
 scheduler.add_jobstore(DjangoJobStore(), "default")
-#하루에 한번 - 우리나라만 대상으로 할 경우 UTC기준 15시에 한번씩 실행되게 하면 됨
-#@register_job(scheduler, "interval", days=1, next_run_time=datetime.datetime.now().replace(hour=15, minute=0, second=0, microsecond=0) + datetime.timedelta(days=1))
-
 #글로벌 대응 - 매 시 정각마다 실행
+#next_run = arrow.utcnow().replace(hour=0, minute=0, second=0, microsecond=0).shift(hours=1).datetime
 #@register_job(scheduler, "interval", hours=1, next_run_time=datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) + datetime.timedelta(hours=1))
 
-#테스트용 - 1분에 한번씩
-@register_job(scheduler, "interval", minutes=1, next_run_time=datetime.datetime.now() + datetime.timedelta(minutes=1), replace_existing=True)
+#테스트용 - 5분에 한번씩
+#next_run = arrow.utcnow().shift(minutes=5).datetime
+#@register_job(scheduler, "interval", minutes=5, next_run_time=next_run, replace_existing=True)
+
+# 하루에 한번 - 우리나라만 대상으로 할 경우 UTC기준 15시/KST기준 00시에 한번씩 실행되게 하면 됨
+next_run = arrow.now('Asia/Seoul').replace(hour=0, minute=0, second=0, microsecond=0).shift(days=1).datetime
+@register_job(scheduler, "interval", days=1, next_run_time=next_run, replace_existing=True)
 def porcess_decrease():
     users = User.objects.all()
     
     for user in users:
         current_time = get_current_arrow(user.tzinfo)   #사용자의 로컬 시간대
+        #if current_time.hour == 0 and current_time.minute == 0: #글로벌 대응용 로직
         
-        #원래 로직
-        #if current_time.hour == 0 and current_time.minute == 0:
-            #previous_day = current_time.shift(days=-1)
-        
-        #테스트용
-        previous_day = current_time
+        previous_day = current_time.shift(days=-1)
         decrease_value(user, previous_day)
     
     return
