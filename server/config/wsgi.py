64ee400016f099ae478422a3e4a714c897b2383c
Ubuntu <ubuntu@ip-172-31-43-43.ap-northeast-2.compute.internal>
2023-08-19 13:43:38 +0000

    release: 배포 파일 수정

--- server/config/wsgi.py
+++ server/config/wsgi.py
@@ -16,9 +16,9 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_wsgi_application()
-
 # 추가
 site.addsitedir(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+application = get_wsgi_application()
 
 from server.apps.main.jobs import scheduler
