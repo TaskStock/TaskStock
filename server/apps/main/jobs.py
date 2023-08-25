025b4d2101703a1f6413e0966c071b4bcd3c5034
ajroot5685 <anjeong8577@naver.com>
2023-08-25 13:33:12 +0900

    release: 알람 오류 수정

--- server/apps/main/jobs.py
+++ server/apps/main/jobs.py
@@ -17,19 +17,7 @@ scheduler.add_jobstore(DjangoJobStore(), "default")
 # 하루에 한번 - 우리나라만 대상으로 할 경우 UTC기준 15시/KST기준 00시에 한번씩 실행되게 하면 됨
 next_run = arrow.now('Asia/Seoul').replace(hour=0, minute=0, second=0, microsecond=0).shift(days=1).datetime
 @register_job(scheduler, "interval", days=1, next_run_time=next_run, replace_existing=True)
-def porcess_midnight():
-    with transaction.atomic():
-        groups = Group.objects.select_for_update().all()
-        for group in groups:
-            group.delta = 0
-            group.save()
-
-    with transaction.atomic():
-        per_users=User.objects.select_for_update().all()
-        for user in per_users:
-            user.percentage = 0
-            user.save()
-    
+def porcess_midnight():    
     users = User.objects.all()
     current_time = get_current_arrow('Asia/Seoul')   
     previous_day = current_time.shift(days=-1)
@@ -53,17 +41,30 @@ def porcess_midnight():
             print(f"Error in alarm_calculate_follow for user {user.id}: {e}")
             continue
 
-        try:
-            alarm_calculate_group()
-        except Exception as e:
-            print(f"Error in alarm_calculate_group: {e}")
-            continue
+    # 그룹과 랭킹 알람은 유저 각각이 실행하면 안됨
+    try:
+        alarm_calculate_group()
+    except Exception as e:
+        print(f"Error in alarm_calculate_group: {e}")
 
-        try:
-            alarm_calculate_ranking()
-        except Exception as e:
-            print(f"Error in alarm_calculate_ranking: {e}")
-            continue
+    try:
+        alarm_calculate_ranking()
+    except Exception as e:
+        print(f"Error in alarm_calculate_ranking: {e}")
+
+
+    # 하루 동안의 결과를 알람으로 보낸 후 업데이트 해야함
+    with transaction.atomic():
+        groups = Group.objects.select_for_update().all()
+        for group in groups:
+            group.delta = 0
+            group.save()
+
+    with transaction.atomic():
+        per_users=User.objects.select_for_update().all()
+        for user in per_users:
+            user.percentage = 0
+            user.save()
 
 
 if not scheduler.running:
